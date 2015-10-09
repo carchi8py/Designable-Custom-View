ebe5038ed83c2d6b96341eb34d8df9214b2cca28
Chris Archibald <carchi8py@gmail.com>
2015-10-08 20:52:23 -0700

    Our custom view now appears

--- Designable Custom View/PopupView.swift
+++ Designable Custom View/PopupView.swift
@@ -9,8 +9,41 @@
 import UIKit
 
 class PopupView: UIView {
-
-    /*
+    
+    var nibName = "PopupView"
+    
+    //When a Programer creates a PopupView
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        setup()
+    }
+    
+    //When Storyboard or UI file uses PopupView
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        var view = loadViewFromNib()
+        //You want to use bounds, because it is the local set of x, y
+        // frame will not work if we move it in the master view
+        view.frame = self.bounds
+        view.autoresizingMask = UIViewAutoresizing.FlexibleHeight.union(UIViewAutoresizing.FlexibleWidth)
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        //This will grab the top level view
+        let view  = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        return view
+    }
+    
+  /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
