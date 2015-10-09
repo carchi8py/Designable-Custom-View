9dbb66d1f5a9f5bbf9d214fc277d1210e44eb21d
Chris Archibald <carchi8py@gmail.com>
2015-10-08 20:30:20 -0700

    Created custom View what we can use.

---
+++ Designable Custom View/PopupView.swift
@@ -0,0 +1,21 @@
+//
+//  PopupView.swift
+//  Designable Custom View
+//
+//  Created by Chris Archibald on 10/8/15.
+//  Copyright © 2015 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class PopupView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
